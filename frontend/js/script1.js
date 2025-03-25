6954ebabb5d63ea332891af00fe358eec9c4742f
Gutijga <98857954+Gutijga@users.noreply.github.com>
2025-03-24 22:53:06 -0500

    Update script1.js

--- frontend/js/script1.js
+++ frontend/js/script1.js
@@ -63,7 +63,7 @@ async function crearSala() {
         let response = await fetch("https://console.clever-cloud.com/users/me/applications/app_e52f1e4d-2eb3-4fd7-8507-8a86fa0e0a67", {
             method: "POST",
             headers: {
-                "Content-Type": "application/json"
+                "Content-Type": application/json
             },
             body: JSON.stringify({ codigo: codigoSala })
         });
